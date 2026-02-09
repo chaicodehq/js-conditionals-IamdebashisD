d2da0ad6e8bc3d68e758444f840b22e5e4e89916
IamdebashisD <adebashisdas626@gmail.com>
2026-02-10 00:41:26 +0530

    Implement password strength checker in checkPasswordStrength function

--- src/09-password-strength.js
+++ src/09-password-strength.js
@@ -27,4 +27,45 @@
  */
 export function checkPasswordStrength(password) {
   // Your code here
+
+  let capLetters = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+    'Y', 'Z'
+  ]
+
+  let smallLetter = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+    'o', 'p', 'q', 'r', 's', 't', 'u', 
+    'v', 'w', 'x', 'y', 'z'
+  ]
+
+  let specialChar = [
+    '!', '@', '#', '$', '%', '^', '&',
+    '*', '(', ')', '_', '+', '-', '=', 
+    '[', ']', '{', '}', '|', ';', ':', 
+    ',', '.', '<', '>', '?'
+  ]
+
+  let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+  if(typeof password !== "string" || password.trim().length === 0) return "weak"
+
+  const isLong = password.length >= 8;
+  const atLeastOneUpperChar = [...password].some(char => capLetters.includes(char))
+  const atLeastOneLowerChar = [...password].some(char => smallLetter.includes(char))
+  const atLeastOneNumber = [...password].some(char => numbers.includes(char))
+  const oneSpecialChar = [...password].some(char => specialChar.includes(char))
+
+  const criteriaArray = [isLong, atLeastOneUpperChar, atLeastOneLowerChar, atLeastOneNumber, oneSpecialChar]
+
+  const criteriaCount = criteriaArray.filter(items => items === true).length
+
+  if (criteriaCount >= 5) return "very strong"
+  if (criteriaCount === 4) return "strong"
+  if (criteriaCount === 2 || criteriaCount === 3) return "medium"
+  if (criteriaCount === 1) return "weak"
 }
